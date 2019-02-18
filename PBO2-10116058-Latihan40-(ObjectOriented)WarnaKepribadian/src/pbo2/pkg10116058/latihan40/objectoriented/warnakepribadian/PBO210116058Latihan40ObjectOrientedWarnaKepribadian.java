b778a517ea28552209c11ef33e7b924a3a9f33ff
nialditanawara <31877365+nialditanawara@users.noreply.github.com>
2019-02-19 01:56:29 +0700

    Add files via upload

---
+++ PBO2-10116058-Latihan40-(ObjectOriented)WarnaKepribadian/src/pbo2/pkg10116058/latihan40/objectoriented/warnakepribadian/PBO210116058Latihan40ObjectOrientedWarnaKepribadian.java
@@ -0,0 +1,24 @@
+package pbo2.pkg10116058.latihan40.objectoriented.warnakepribadian;
+
+
+import pbo2.pkg10116058.latihan40.objectoriented.warnakepribadian.Warna;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class PBO210116058Latihan40ObjectOrientedWarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Warna doyen = new Warna();
+        
+        doyen.ioWarnaMu();
+    }
+    
+}
